4545eb0030e306f34f6a32901c558c832649cdc6
Azam <gulovazam@gmail.com>
2022-12-12 21:59:50 +0500

    Add function that returns gameBoard array
    Add render function that populates gameboard

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 const Gameboard = (() => {
-  const gameBoard = [];
+  const gameBoard = ["x", "o", "x", "o", "x", "o", "x", "x", ""];
+  const getGameBoard = () => gameBoard;
+
+  return {
+    getGameBoard,
+  };
 })();
-const displayController = (() => {})();
+
+const displayController = ((e) => {
+  const domGameBoard = document.getElementById("gameBoard");
+  const cells = domGameBoard.querySelectorAll(".cell");
+
+  const render = () => {
+    const gameBoard = Gameboard.getGameBoard();
+    for (i = 0; i < cells.length; i++) {
+      cells[i].textContent = gameBoard[i];
+    }
+  };
+
+  return {
+    render,
+  };
+})();
+
 const Player = (name) => {};
+
+displayController.render();
