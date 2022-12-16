db762a8d83a2b7eeeb6292eecc149bd5b324d100
Azam <gulovazam@gmail.com>
2022-12-16 22:53:35 +0500

    Add an array of all possible winning patterns

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const gameBoard = (() => {
   const board = new Array(9).fill("");
+  const winPatterns = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+  ];
   const getGameBoard = () => [...board];
 
   const setCell = (position, marker) => {
